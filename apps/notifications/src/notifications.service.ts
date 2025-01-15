6eff4f4b8ad44f47faaa01e387b75ce99b0a3011
MaxChou1205 <v2asault@yahoo.com.tw>
2025-01-15 22:09:47 +0800

    add send email service

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { NotifyEmailDto } from './dto/notify-email.dto';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class NotificationsService {
-  async notifyEmail(data: NotifyEmailDto) {
-    console.log(data);
+  constructor(private configService: ConfigService) {}
+
+  private readonly transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      // type: 'OAuth2',
+      // user: this.configService.get('EMAIL_USER'),
+      // clientId: this.configService.get('GMAIL_OAUTH_CLIENT_ID'),
+      // clientSecret: this.configService.get('GMAIL_OAUTH_CLIENT_SECRET'),
+      // refreshToken: this.configService.get('GMAIL_OAUTH_REFRESH_TOKEN'),
+      user: this.configService.get('GMAIL_USER'),
+      pass: this.configService.get('GMAIL_PASSWORD'),
+    },
+  });
+
+  async notifyEmail({ email }: NotifyEmailDto) {
+    await this.transporter.sendMail({
+      from: this.configService.get('GMAIL_USER'),
+      to: email,
+      subject: 'Hello from NestJS',
+      text: 'Hello world!',
+    });
   }
 }
